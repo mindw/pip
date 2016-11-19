de2f0bb84d6d7e781826504f066367f4392f208d
Gabi Davar <grizzly.nyo@gmail.com>
2016-11-19 15:14:46 +0200

    list get not requires examines extras.

--- pip/commands/list.py
+++ pip/commands/list.py
@@ -181,6 +181,9 @@ class ListCommand(Command):
         dep_keys = set()
         for dist in packages:
             dep_keys.update(requirement.key for requirement in dist.requires())
+            for e in dist.extras:
+                dep_keys.update(
+                    requirement.key for requirement in dist.requires([e]))
         return set(pkg for pkg in packages if pkg.key not in dep_keys)
 
     def iter_packages_latest_infos(self, packages, options):
